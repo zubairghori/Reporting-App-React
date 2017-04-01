83531344f689cea0bd71272fa11d287f5268d2a2
Admin <zubairghori93@gmail.com>
2017-04-01 19:22:14 +0500

    changes

--- src/store/index.js
+++ src/store/index.js
@@ -16,9 +16,7 @@ export {
 //const middleware = applyMiddleware(thunk,logger());
 const middleware = compose(
       applyMiddleware(thunk,logger())
-      ,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+          );
 export const rootReducer = combineReducers({
     AuthReducer,
     ReportReducer
